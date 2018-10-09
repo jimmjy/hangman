4128bea6de26911769b81f20c53b57eaa27d8ccf
James Finkelstein <jfinkelstein9@me.com>
2018-10-09 13:26:15 -0400

    update app.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 const game1 = new Hangman('cat', 2);
 
-// Guess c, t, z
-// Print remaining guesses (should be 1)
-const game2 = new Hangman('New Jersey', 4);
-// Guess w, 
-game2.getPuzzle();
-
 window.addEventListener('keypress', function (e) {
     document.getElementsByTagName('body').innerText = '';
     const guess = String.fromCharCode(e.charCode);
