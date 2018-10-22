04f4b24d63daa64a8bb76eb562684842896cf6f9
James Finkelstein <jfinkelstein9@me.com>
2018-10-22 18:58:52 -0400

    updated security

--- requests.js
+++ requests.js
@@ -1,6 +1,6 @@
 //api request for random words
 const getPuzzle = async (wordCount) => {
-    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
+    const response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
     if (response.status === 200) {
         const data = await response.json();
         return data.puzzle;
