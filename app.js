841bf81c9ec46e964e4fde5ea629fb7fe30ebbeb
James Finkelstein <jfinkelstein9@me.com>
2018-10-22 18:48:45 -0400

    updated changes, now working

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 // default variables
-const game1 = new Hangman('car parts', 2);
-const puzzle = document.getElementById('puzzle');
+let game1;
+const puzzleEl = document.getElementById('puzzle');
 const guesses = document.getElementById('guesses');
 
 // configure inital DOM with inforation for user to see
-puzzle.textContent = game1.puzzle;
-guesses.textContent = game1.statusMessage;
 
 //recognize user input pressed on display
 window.addEventListener('keypress', function (e) {
     const guess = String.fromCharCode(e.charCode);
     game1.makeGuess(guess);
-    puzzle.textContent = game1.puzzle;
+    render();
+});
+
+const startGame = async () => {
+    const puzzle = await getPuzzle(2);
+    game1 = new Hangman(puzzle, 4);
+    render();
+};
+
+document.querySelector('#reset').addEventListener('click', () => {startGame();});
+
+startGame();
+
+const render = () => {
+    puzzleEl.textContent = game1.puzzle;
     guesses.textContent = game1.statusMessage;
-});
+};
 
-getPuzzle(2).then((word) => {
-    console.log(word);
-}).catch((err) => {
-    console.log(err);
-});
-
-
+// getPuzzle(2).then((word) => {
+//     console.log(word);
+// }).catch((err) => {
+//     console.log(err);
+// });
 
 
 
